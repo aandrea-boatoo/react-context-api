8f12f263960b7a10d728c967e0941beaa63f6b35
Boato Andrea <163646394+aandrea-boatoo@users.noreply.github.com>
2025-01-13 18:19:16 +0100

    context

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import FooterComponent from './Components/FooterComponent'
+import HeaderComponent from './Components/HeaderComponent'
+import MainComponent from './Components/MainComponent'
+
+function App() {
+  return (
+    <>
+      <HeaderComponent />
+      <MainComponent />
+      <FooterComponent />
+    </>
+  )
+}
+
+export default App
